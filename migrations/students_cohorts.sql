a755fa94e7214f48e5589cfaedc35bc1b01fe689
Connor <cgbroadfoot@gmail.com>
2023-02-07 02:19:55 +0000

    Add two new tables with dummy data. Add one for assignments and one for assignment submissions

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,4 +1,17 @@
-SELECT id, name, email, cohort_id 
-FROM students
-WHERE github IS NULL
-ORDER BY cohort_id;
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
