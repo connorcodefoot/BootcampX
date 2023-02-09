b66784a1dbdac955e9f768a063402ef545d94f74
Connor <cgbroadfoot@gmail.com>
2023-02-09 04:27:15 +0000

    Add db connection to server. Add command line interactive queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2]
+const values = [`${cohortName}`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER by teachers.name;
-`)
+`, values)
   .then(res => {
     console.log(res.rows);
   })
